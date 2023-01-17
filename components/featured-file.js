23c9b3a784b3dbd8fa27b0fdabbd1afaef32db5f
Eric Price <info@ericprice.info>
2023-01-17 00:43:14 +0000

    cleanup

--- components/featured-file.js
+++ components/featured-file.js
@@ -4,8 +4,7 @@ import { sanityConfig } from '../lib/config'
 
 export default function CoverFile({ file }) {
   const theFile = getFile(file, sanityConfig)
-  console.log('yup')
-
+  
   return (
     <div className="project-meta-header-item project-meta-header-media">
       <video type="video/mp4" src={theFile.asset.url} autoPlay muted loop />
