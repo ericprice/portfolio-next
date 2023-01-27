7c94a3b8900a8c02dd6bf83b5f9f14cbd03dcf89
Eric Price <info@ericprice.info>
2023-01-27 22:42:02 +0000

    add google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -9,6 +10,18 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-PLJECFNMR0"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-PLJECFNMR0');
+          `}
+        </Script>
       </body>
     </Html>
   )
