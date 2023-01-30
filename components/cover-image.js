94edc9d62d8417b90e43b4a639674543d465a61d
Eric Price <info@ericprice.info>
2023-01-30 00:01:11 +0000

    bug fix (and better solution anyway)

--- components/cover-image.js
+++ components/cover-image.js
@@ -3,12 +3,10 @@ import { urlForImage } from '../lib/sanity'
 
 export default function CoverImage({ image: source, priority }) {
   const image = source?.asset?._ref ? (
-    <div className="project-preview-thumbnail-container">
-      <div
-        className="project-preview-thumbnail"
-        style={{ backgroundImage: `url(${urlForImage(source).width(2000).quality(85).url()})` }}
-      >
-      </div>
+    <div
+      className="project-preview-thumbnail"
+      style={{ backgroundImage: `url(${urlForImage(source).width(2000).quality(85).url()})` }}
+    >
     </div>
   ) : (
     <></>
