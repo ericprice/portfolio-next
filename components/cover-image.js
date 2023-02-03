f2bb47a3281eba8a93d44b62eee8a88f7d1a039c
Eric Price <info@ericprice.info>
2023-02-03 22:01:46 +0000

    set once on lazyload

--- components/cover-image.js
+++ components/cover-image.js
@@ -3,7 +3,7 @@ import { urlForImage } from '../lib/sanity'
 
 export default function CoverImage({ image: source, priority }) {
   const image = source?.asset?._ref ? (
-    <LazyLoad offset={600}>
+    <LazyLoad offset={600} once>
       <div
         className="project-preview-thumbnail"
         style={{ backgroundImage: `url(${urlForImage(source).width(2000).quality(85).url()})` }}
--- components/featured-file.js
+++ components/featured-file.js
@@ -7,7 +7,7 @@ export default function CoverFile({ file }) {
   
   return (
     <div className="project-meta-header-item project-meta-header-media">
-      <LazyLoad offset={600}>
+      <LazyLoad offset={600} once>
         <video loop={true} autoPlay={true} muted={true} playsInline={true}>
           <source src={theFile.asset.url} type="video/mp4"></source>
         </video>
