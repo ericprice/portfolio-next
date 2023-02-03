d335fab6507edd3ef70b0b74b55c73cdac861c9a
Eric Price <info@ericprice.info>
2023-02-03 21:49:29 +0000

    put image dimensions on homepage

--- components/featured-image.js
+++ components/featured-image.js
@@ -1,9 +1,19 @@
 import { urlForImage } from '../lib/sanity'
+import { getImageDimensions } from '@sanity/asset-utils'
 
 export default function FeaturedImage({ image: source, priority }) {
+  
+  if (source?.asset?._ref) {
+    var { aspectRatio, height, width } = getImageDimensions({
+      url: urlForImage(source).width(1400).quality(75).url()
+    })
+    
+    var trueHeight = 1400 / aspectRatio;
+  }
+  
   const image = source?.asset?._ref ? (
     <div className="project-meta-header-item project-meta-header-media">
-      <img src={urlForImage(source).width(1400).quality(75).url()} alt="" loading="lazy" />
+      <img src={urlForImage(source).width(1400).quality(75).url()} alt="" loading="lazy" width="1400" height={trueHeight} />
     </div>
   ) : (
     <></>
